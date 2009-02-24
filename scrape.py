1a8a9a874f5fb2616cf2089101e275490d94cb4f
Antti Rasinen <ars@iki.fi>
2009-02-24 21:43:00 +0200

    What an embarrassing bug

--- scrape.py
+++ scrape.py
@@ -9,9 +9,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-    <p id="footer" style="color:#666; font-size:0.9em; ">
-            3.451.108 registered users. Last updated 20:32:04.<br />
-            IPv4 22.641.835 peers (10.558.456 seeders + 12.083.379 leechers) in 1.696.749 torrents on tracker.<br />
-            IPv6 22.663 peers (9.603 seeders + 13.060 leechers) in 21.763 torrents on tracker.<br />
-    </p>
